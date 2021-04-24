d998b78f5aae9c2201bb90ef0c06062a0fd42173
RajveerSingh10 <75128704+RajveerSingh10@users.noreply.github.com>
2021-04-24 16:33:34 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+Class Paper(){
+    Matter.js(circular);
+
+    var options{
+        isStatic:false,
+        resittution:0.3,
+        friction:0,
+        density:1.2
+        Image();
+
+
+    }
+    paper =loadImage("paper.png");
+    Image.display(paper.png)
+
+
+}
+
+function keyPressed(){
+if(keyCode==UP_ARROW){
+    Master.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+}
+}
